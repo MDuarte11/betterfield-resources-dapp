261c339da7561b40ad132ac25355188624a4d9b9
MDuarte11 <marcopduarte11@gmail.com>
2023-02-28 19:07:18 +0000

    [Backend][SC] Define BFAccessControl smart contract

---
+++ backend/smart-contracts/BFAccessControl.sol
@@ -0,0 +1,10 @@
+// smart-contracts/BFAccessControl.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract BFAccessControl is AccessControl {
+
+}
+
