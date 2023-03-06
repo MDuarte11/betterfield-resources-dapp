9cd5d20f7f182a77023e535b2567a8dec1278b17
MDuarte11 <marcopduarte11@gmail.com>
2023-03-06 21:39:57 +0000

    [Backend][SC] Improve the AccessControl smart contract by adding getters, an unregister function and improving the validateRegistration function

--- backend/contracts/BFAccessControl.sol
+++ backend/contracts/BFAccessControl.sol
@@ -8,10 +8,20 @@ contract BFAccessControl is AccessControl {
     // Users that want to register to the service
     mapping (address => bool) registeringUsers;
     // Currently registered users
-    mapping (address => bool) public users;
+    mapping (address => bool) users;
 
     constructor(address firstUser) {
         users[firstUser] = true;
+    }
+
+    // A public getter to check if an user is registered
+    function getUser(address account) public view returns (bool) {
+        return users[account];
+    }
+
+    // A public getter to check if an user is in the registration process
+    function getRegisteringUser(address account) public view returns (bool) {
+        return registeringUsers[account];
     }
 
     modifier onlyUsers() {
@@ -27,12 +37,21 @@ contract BFAccessControl is AccessControl {
 
     // A publicly accessible function that takes an address to start the registration process
     function register(address userAddress) public {
-        registeringUsers[userAddress] = false;
+        registeringUsers[userAddress] = true;
     }
 
+    // A publicly accessible function that takes an address to unregister an user by setting its active status to false,
+    // but keeps the address for history purposes
+    function unregister(address userAddress) onlyUsers public {
+        users[userAddress] = false;
+    }
+
+    // A public function to let a current user accept an another account registration
     function validateRegistration(address userAddress) onlyUsers public {
-        registeringUsers[userAddress] = true;
-        users[userAddress] = true;
+        require(registeringUsers[userAddress], 'The provided account is not queued to be registered.');
+        
+        delete registeringUsers[userAddress];
+        users[userAddress] = true;     
     }
 }
 
