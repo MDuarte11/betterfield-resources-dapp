4fd08513d17898c211a2bcac74ea0907719ef7d8
MDuarte11 <marcopduarte11@gmail.com>
2023-03-07 21:21:37 +0000

    [Backend][Hardhat] Implement register task to queue an account for the registration process

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -2,8 +2,39 @@
 
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
+var fs = require('fs');
+const util = require('util');
+var ethers = require('ethers')
+const fsPromises = fs.promises;
 
 const { API_URL, PRIVATE_KEY } = process.env;
+const ACCESS_CONTROL_ABI_FILE_PATH = "artifacts/contracts/BFAccessControl.sol/BFAccessControl.json";
+
+async function getAbi(abi_path) {
+    const data = await fsPromises.readFile(abi_path, 'utf8');
+    const abi = JSON.parse(data)['abi'];
+    return abi;
+}
+
+task("register", "Starts the registration process of an account")
+.addParam("smartcontractaddress", "The deployed AccessControl smart contract address")
+.addParam("accountaddress", "The account address to be registered")
+.setAction(async (taskArgs) => {
+   // Setup and check that address is neither as user or a registeringUser
+   const {API_URL} = process.env
+   let provider = ethers.getDefaultProvider(API_URL)
+   const abi = await getAbi(ACCESS_CONTROL_ABI_FILE_PATH)
+
+   const { PRIVATE_KEY } = process.env
+   let signer = new ethers.Wallet(PRIVATE_KEY, provider)
+   const access_control_contract = new ethers.Contract(taskArgs.smartcontractaddress, abi, signer)
+
+   // Queue address to be registered
+   let register_transaction = await access_control_contract.register(taskArgs.accountaddress)
+   await register_transaction.wait()
+   const queuedForRegistration = await access_control_contract.getRegisteringUser(taskArgs.accountaddress)
+   console.log(`Account queued for registration: ${queuedForRegistration}`)
+});
 
 module.exports = {
    solidity: "0.8.9",
