412ce2f606add2ddbefa10b3f4a16fa956ff7307
MDuarte11 <marcopduarte11@gmail.com>
2023-04-03 18:49:05 +0100

    [Backend][SC] Update the deploy script to deploy the BFWriteInspection contract using the BFAccessControl and the BFWriteResource addresses

--- backend/scripts/deploy.js
+++ backend/scripts/deploy.js
@@ -10,6 +10,12 @@ async function main() {
     // Start deployment, returning a promise that resolves to a contract object
     const write_resource = await BFWriteResource.deploy(access_control.address);   
     console.log("Write Resource contract deployed to address:", write_resource.address);
+
+    const BFWriteInspection = await ethers.getContractFactory("BFWriteInspection");
+ 
+    // Start deployment, returning a promise that resolves to a contract object
+    const write_inspection = await BFWriteInspection.deploy(access_control.address, write_resource.address);   
+    console.log("Write Inspection contract deployed to address:", write_inspection.address);
  }
  
  main()
