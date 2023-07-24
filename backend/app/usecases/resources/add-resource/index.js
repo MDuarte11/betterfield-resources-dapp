1c3e2b583b0ac0309a32553aa8a9054d9f212822
MDuarte11 <marcopduarte11@gmail.com>
2023-07-24 21:58:55 +0100

    [WIP] Convert geoloaction coordinates to WGS84

--- backend/app/usecases/resources/add-resource/index.js
+++ backend/app/usecases/resources/add-resource/index.js
@@ -1,12 +1,29 @@
 const hrd = require('hardhat');
+const geolib = require('geolib');
 
 async function run(smartContractAddress, resourceId, resource) {
+    let originalLocation = resource.location
+    let formattedLocation = originalLocation
+    if(originalLocation) {
+      formattedLocation = convertCoordinatesToWGS84(originalLocation)
+    }
+    resource.location = formattedLocation
     const resourceAdded = await hrd.run("add-resource", {
         smartcontractaddress: smartContractAddress,
         resourceid: resourceId,
         resource: JSON.stringify(resource)
     });
     return typeof resourceAdded !== "undefined"
+}
+
+async function convertCoordinatesToWGS84(coordinates) {
+  try {
+    const convertedCoordinates = await geolib.getLatLon(coordinates);
+    return convertedCoordinates;
+  } catch (error) {
+    console.error('Error converting coordinates to WGS84:', error);
+    throw new Error('Failed to convert coordinates');
+  }
 }
 
 module.exports = {
